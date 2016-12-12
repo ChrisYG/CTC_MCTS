2bbb61583d903c7669a0a8ed57a48c5f9fac50c1
Chris <yg012014@mymail.pomona.edu>
2016-12-11 21:05:58 -0800

    Finish CircleTheCatAction class and the basics of CircleTheCat class

--- src/circle_the_cat/CircleTheCat.java
+++ src/circle_the_cat/CircleTheCat.java
@@ -1,24 +1,62 @@
 package circle_the_cat;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 import mctslib.game.State;
 
 
 public class CircleTheCat extends State<CircleTheCatAction> {
-	private int numPlayers;
-	private int state;
-	private int turn;
-	private int winner;
+	private int [][] state;
+	private int row, col; // initial position for cat
+	private int numBlocks; // initial number of blocks
+	private int turn; // 0 for human's turn and 1 for cat's turn
+	private int winner; // 0 for human, 1 for cat, -1 for non-available
 	
-	public CircleTheCat(int np) {
-		numPlayers = np;
-		state = 0;
+	public CircleTheCat() {
+		state = new int[9][9];
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				state[i][j] = 0;
+			}
+		}
+		row = 4;
+		col = 4;
+		numBlocks = 9;
 		turn = 0;
 		winner = -1;
+		initialize();
+	}
+	/**
+	 * Initialize the map
+	 * Randomly place blocks as 1, and cat as 8 on the map
+	 */
+	public void initialize() {
+		Random rand = new Random();
+		Set<Integer> checkSet = new HashSet<Integer>();
+		for (int count = 0; count < this.numBlocks; count++) {
+			int randPoint = rand.nextInt(81) + 1; 
+			while (checkSet.contains(randPoint) || randPoint == 41) {
+				randPoint =rand.nextInt(81) + 1; // If point's been added or at cat's location
+			}
+			checkSet.add(randPoint);
+			int row = randPoint / 9;
+			if (row == 9)
+				row = 8;
+			int col = randPoint % 9;
+			if (col == 0)
+				col = 8;
+			else
+				col -= 1;
+			this.state[row][col] = 1;
+		}
+		this.state[4][4] = 8; // 神经猫的初始站位
 	}
 	
 	public int getWinner() {
@@ -32,25 +70,28 @@ public class CircleTheCat extends State<CircleTheCatAction> {
 
 	@Override
 	public State<CircleTheCatAction> getDeepCopy() {
-		CircleTheCat ret = new CircleTheCat(numPlayers);
-		ret.state = state;
-		ret.turn = turn;
-		ret.winner = winner;
-		return ret;
+		CircleTheCat copy = new CircleTheCat();
+		copy.state = state.clone(); // Deep copying the matrix?
+		copy.row = row;
+		copy.col = col;
+		copy.turn = turn;
+		copy.winner = winner;
+		return copy;
 	}
 
 	@Override
 	public void process(CircleTheCatAction action) {
-		state += action.get();
+		if (action.getActor() == 0) { // A human's action
+			
+			
+		} else { // A cat's action
+			
+		}
 		
 		if (isTerminal()) {
 			winner = turn;
 		}
 		
-		turn += 1;
-		if (turn >= numPlayers) {
-			turn = 0;
-		}
 	}
 
 	@Override
--- src/circle_the_cat/CircleTheCatAction.java
+++ src/circle_the_cat/CircleTheCatAction.java
@@ -3,14 +3,20 @@ package circle_the_cat;
 import mctslib.game.Action;
 
 public class CircleTheCatAction extends Action {
-	int number;
-	public CircleTheCatAction() {
+	private int row, col; // point to be moved to
+	private int actor; // 0 for human's turn, 1 for cat's turn
+	public CircleTheCatAction(int actor, int row, int col) {
 		// TODO Auto-generated constructor stub
+		this.set(actor, row, col);
 	}
 
 	@Override
 	public boolean equals(Object o) {
 		// TODO Auto-generated method stub
+		if (o instanceof CircleTheCatAction) {
+			CircleTheCatAction c = (CircleTheCatAction) o;
+			return row == c.row && col == c.col && actor == c.actor;
+		}
 		return false;
 	}
 
@@ -19,21 +25,33 @@ public class CircleTheCatAction extends Action {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	public void set(int n) {
-		if (n != 1 && n != 2) {
+	public void set(int actor, int row, int col) {
+		if ((actor != 0 && actor != 1) || row < 1 || row > 9 || col < 1 || col > 9) {
 			throw new IllegalArgumentException();
 		}
-		number = n;
+		this.actor = actor;
+		this.row = row;
+		this.col = col;
 	}
 	
-	public int get() {
-		return number;
+	public int getActor() {
+		return actor;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getCol() {
+		return col;
 	}
 
-
-	
 	@Override
 	public String toString() {
-		return ""+number;
+		if (actor == 0) {
+			return "You blocked Row " + row + " Col " + col;
+		} else {
+			return "Cat went to Row " + row + " Col " + col;
+		}
 	}
 }
